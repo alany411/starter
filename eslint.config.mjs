b0853619a1f6463b38d5f778250c83c104ee2d7b
alany411 <alanyang411@gmail.com>
2025-03-05 15:21:29 -0500

    feat: add simple-import-sort

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,8 @@
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
 import prettierPluginConfig from 'eslint-plugin-prettier/recommended'
+import simpleImportSortPlugin from 'eslint-plugin-simple-import-sort'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -14,6 +15,18 @@ const nextConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
 ]
 
+const simpleImportSortConfig = [
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSortPlugin,
+    },
+    rules: {
+      'simple-import-sort/exports': 'error',
+      'simple-import-sort/imports': 'error',
+    },
+  },
+]
+
 const prettierConfig = [prettierPluginConfig]
 
-export default [...nextConfig, ...prettierConfig]
+export default [...nextConfig, ...simpleImportSortConfig, ...prettierConfig]
